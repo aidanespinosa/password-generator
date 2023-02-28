c92f94dfec51b4daf08fcb796643086230acd468
aidanespinosa <aidanespinosa3@gmail.com>
2023-02-27 16:31:23 -0800

    made it better

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,4 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+// Get references to the #generate element and the modal elements
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -10,8 +7,47 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function () {
+  writePassword();
+});
+
+// Generate password based on selected criteria
+function generatePassword() {
+  var length = parseInt(
+    prompt("How many characters do you want your password to be?")
+  );
+  var lowercase = confirm("Do you want to include lowercase letters?");
+  var uppercase = confirm("Do you want to include uppercase letters?");
+  var numbers = confirm("Do you want to include numbers?");
+  var symbols = confirm("Do you want to include symbols?");
+
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numberChars = "0123456789";
+  var symbolChars = "!@#$%^&*()_-+=";
+
+  var allChars = "";
+  if (lowercase) {
+    allChars += lowercaseChars;
+  }
+  if (uppercase) {
+    allChars += uppercaseChars;
+  }
+  if (numbers) {
+    allChars += numberChars;
+  }
+  if (symbols) {
+    allChars += symbolChars;
+  }
+
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * allChars.length);
+    password += allChars[randomIndex];
+  }
+
+  return password;
+}
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Password Generator Starter Code
+# Password Generator
+
+A password generator using java script
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributors](#contribute)
+- [Tests](#tests)
+- [Questions](#questions)
+
+  # Installation
+
+  N/A
+
+## Usage
+
+Just follow the prompt!
+
+## License
+
+The MIT License
+
+## Contributors
+
+Aidan Espinosa
+
+## Tests
+
+Jest Node Module
+
+## Questions
+
+- Git Hub: https://github.com/aidanespinosa
+- Email: aidanespinosa3@gmail.com
+
+## Link to how-to-video
+
+N/A
